a26f76def8b907bbefb5264aa4be7151bbcc2685
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-10 03:07:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  index=0
+  array.each do |word|
+    index+=1
+    puts "#{index}. #{word}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect do |plant|
+   plant.capitalize.concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
