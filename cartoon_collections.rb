cfb4aaff37fba41215dd1cb025a1bb1b5adab3d1
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-10 03:16:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  index=0
+  array.each do |word|
+    index+=1
+    puts "#{index}. #{word}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect do |plant|
+   plant.capitalize.concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? do |word|
+    word.length>4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+     cheese == cheese_types[0] || cheese == cheese_types[1] ||  cheese == cheese_types[2]
+    end
+  end
 end
